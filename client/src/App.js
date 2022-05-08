5db516ff9dd90cf362d11e95639d2071975155bc
Abeeb Ahmed <abeebahmed@gmail.com>
2022-05-08 01:09:04 +0100

    code edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Topbar from "./components/topbar/Topbar";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
-import Feed from "../components/feed/Feed";
-import Leftbar from "../components/leftbar/Leftbar";
-import Rightbar from "../components/rightbar/Rightbar";
+import Feed from "../../components/feed/Feed";
+import Leftbar from "../../components/leftbar/Leftbar";
+import Rightbar from "../../components/rightbar/Rightbar";
 import "./home.scss";
 
 const Home = () => {
