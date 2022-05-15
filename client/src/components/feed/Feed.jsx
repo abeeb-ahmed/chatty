02e19f7d3669980094d0c4a6fc0a26ac88b103fb
Abeeb Ahmed <abeebahmed@gmail.com>
2022-05-15 20:24:58 +0100

    display followings

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -6,7 +6,7 @@ import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.scss";
 
-const Feed = () => {
+const Feed = ({ username }) => {
   const { user } = useSelector((state) => state.auth);
   const [posts, setPosts] = useState([]);
   const userId = user._id;
@@ -14,20 +14,20 @@ const Feed = () => {
   useEffect(() => {
     // Get timeline from database and sort
     const getTimeline = async (userId) => {
-      const res = await axios.get(
-        `http://localhost:5000/api/posts/timeline/${userId}`
-      );
+      const res = username
+        ? await axios.get(`http://localhost:5000/api/posts/profile/${username}`)
+        : await axios.get(`http://localhost:5000/api/posts/timeline/${userId}`);
 
       setPosts(
         res.data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
       );
     };
     getTimeline(userId);
-  }, [userId]);
+  }, [userId, username]);
 
   return (
     <div className="feed">
-      <Share />
+      {(!username || username === user?.username) && <Share />}
       {posts.map((post) => {
         return <Post key={post._id} post={post} />;
       })}
